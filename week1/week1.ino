4153ec4ba12a6efbe95c2100da6771a6302952da
Shirley Wu <shirleyxueyangwu@gmail.com>
2022-02-01 22:39:31 -0500

    finish week1 homework

--- week1/week1.ino
+++ week1/week1.ino
@@ -1,15 +1,32 @@
+#include <Arduino_JSON.h>
+#include <Encoder.h>
+
+Encoder myEncoder(2, 3);
+
 // set up pin numbers for echo pin and trigger pins:
-const int trigPin = 2;
-const int echoPin = 3;
+const int trigPin = 11;
+const int echoPin = 12;
+const int encoderPin = 4;
+const int fps = 16;
 int prevDistance = 0;
+int prevTurn = 0;
+
+// JSON object
+JSONVar outgoing;
 
 void setup() {
   // set the modes for the trigger pin and echo pin:
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  // encoder's pushbutton
+  pinMode(encoderPin, INPUT_PULLUP);
   // initialize serial communication:
   Serial.begin(9600);
 
+  outgoing["time"] = 0;
+  outgoing["sensor"] = 0;
+  outgoing["button"] = 0;
+  outgoing["turn"] = 0;
 }
 
 void loop() {
@@ -29,7 +46,7 @@ void loop() {
   // calculate the distance in cm.
   //Sound travels approx.0.0343 microseconds per cm.,
   // and it's going to the target and back (hence the /2):
-  int distance = (duration * 0.0343) / 2;
+  float distance = (duration * 0.0343) / 2;
 
   if (distance >= 1000) {
     // sometimes sensor will give a faulty reading when
@@ -37,13 +54,19 @@ void loop() {
     // so set it to previous reading
     distance = prevDistance;
   }
-  // then constrain to 0 to 50cm
-  distance = constrain(distance, 0, 50);
+  // then constrain to 0 to 20cm
+  distance = constrain(distance, 0, 20);
+
+  outgoing["time"] = millis();
+  outgoing["sensor"] = distance;
+  outgoing["button"] = !digitalRead(encoderPin);
+  long turn = myEncoder.read();
+  outgoing["turn"] = turn - prevTurn;
+  Serial.println(outgoing);
 
   prevDistance = distance;
-
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  // a short delay between readings:
-  delay(10);
+  prevTurn = turn;  
+  
+  // a short delay between readings (16ms for 64fps):
+  delay(fps);
 }
