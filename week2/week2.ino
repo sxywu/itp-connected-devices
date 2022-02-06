6011b725c5edf3b032bf9b72db95acd3ef9a180d
Shirley Wu <shirleyxueyangwu@gmail.com>
2022-02-05 23:44:24 -0500

    add tft & comments to week2 oled code

--- week2/week2.ino
+++ week2/week2.ino
@@ -1,12 +1,39 @@
+/*
+ * Adapts TFT ST7789 example from:
+ * https://github.com/tigoe/display-examples/blob/main/TFT_Examples/ST7789/ST7789_Text_Example/ST7789_Text_Example.ino
+ * As well as OLED SSD1306 example:
+ * https://github.com/tigoe/display-examples/blob/main/OLED_Examples/SSD1306_OLED_Example/SSD1306_OLED_Example.ino
+ */
+
 #include <Wire.h>
+// load Adafruit libraries
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
  
 const int SCREEN_WIDTH = 128; // OLED display width, in pixels
 const int SCREEN_HEIGHT = 64; // OLED display height, in pixels
+
+// TFT control pins:
+// setting CS to -1 because of MakerFocus 1.3" not having CS pin
+const int TFT_CS =  -1;
+const int TFT_RST = 9;
+const int TFT_DC = 8;
+// TFT screen size
+const int TFT_WIDTH = 240;
+const int TFT_HEIGHT = 240;
  
 // initialize the oled:
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT);
+// initialize tft
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+// offscreen buffer for TFT which has a really slow refresh rate
+GFXcanvas1 canvas(80, 80);
+
+// font colors
+int fontColor = 0x6677FF;  // light blue
+int bgColor = 0x0;         // black
 
 void setup() {
   // initialize serial and wait for serial monitor to open:
@@ -20,13 +47,32 @@ void setup() {
     Serial.println("oled setup failed");
     while (true);
   }
+
+  // initialize tft with SPI_MODE3 bc device doesn't have CS pin
+  tft.init(TFT_WIDTH, TFT_HEIGHT, SPI_MODE3);
+  tft.setRotation(2);
   
   oled.setTextSize(2);
   oled.setTextColor(SSD1306_WHITE);
+
+  // clear the tft:
+  tft.fillScreen(bgColor);
+  // set the text color to teal:
+  tft.setTextColor(fontColor);
+  tft.setTextSize(4);
+  canvas.setTextSize(4);
+  
+  // print unchanging text
+  tft.setCursor(0, 30);
+  tft.println("secs:");
+  
   Serial.println("oled is good to go");
+  Serial.println("tft is good to go");
 }
 
 void loop() {
+
+  // first display OLED
   oled.clearDisplay();
 
   oled.setCursor(0, 0);
@@ -34,4 +80,20 @@ void loop() {
   oled.print(millis() / 1000);
 
   oled.display();
+
+  delay(10);
+
+  // then TFT
+  // clear the canvas with a rect in the background color:
+  canvas.fillRect(0, 0, canvas.width(), canvas.height(), bgColor);
+  // move the cursor to 0,30 (because fonts measure from their baseline):
+  canvas.setCursor(0, 30);
+  // print the seconds:
+  canvas.println(millis() / 1000);
+  // update the tft with the canvas:
+  tft.drawBitmap(120, 0, canvas.getBuffer(),
+                     canvas.width(), canvas.height(),
+                     fontColor, bgColor);
+
+  delay(10);
 }
