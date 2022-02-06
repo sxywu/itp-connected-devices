586204f6134b458cdb7d664c70cccee50acbb973
Shirley Wu <shirleyxueyangwu@gmail.com>
2022-02-05 23:18:38 -0500

    week2: rename display to oled

--- week2/week2.ino
+++ week2/week2.ino
@@ -5,8 +5,8 @@
 const int SCREEN_WIDTH = 128; // OLED display width, in pixels
 const int SCREEN_HEIGHT = 64; // OLED display height, in pixels
  
-// initialize the display:
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
+// initialize the oled:
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 void setup() {
   // initialize serial and wait for serial monitor to open:
@@ -16,21 +16,21 @@ void setup() {
   // SSD1306_SWITCHCAPVCC is for 3.3V
   // second parameter is I2C address, which is
   // 0x3C, or 3D for some 128x64 modules:
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println("Display setup failed");
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("oled setup failed");
     while (true);
   }
-  Serial.println("Display is good to go");
+  Serial.println("oled is good to go");
 }
 
 void loop() {
-  display.clearDisplay();
-  display.setTextSize(4);
-  display.setTextColor(SSD1306_WHITE);
+  oled.clearDisplay();
+  oled.setTextSize(4);
+  oled.setTextColor(SSD1306_WHITE);
 
-  display.setCursor(0, 0);
-  display.print("secs:");
-  display.print(millis() / 1000);
+  oled.setCursor(0, 0);
+  oled.print("secs:");
+  oled.print(millis() / 1000);
 
-  display.display();
+  oled.display();
 }
